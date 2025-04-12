6ffe313df2a087661ee9c1e55f62580fcbd6dcf2
Jarek Lin <s410673074@gmail.com>
2025-04-12 18:50:45 +0800

    feat: add sui func

---
+++ eslint.config.mjs
@@ -0,0 +1,21 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  { 
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    rules: {
+      "no-unused-vars": "error",
+      "no-undef": "error",
+      "no-console": "warn",
+      "no-debugger": "warn"
+    }
+  },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  { ignores: ["**/_generated"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,66 @@
+import { EnokiClient, EnokiFlow } from "@mysten/enoki";
 import "./style.css";
-import { getWallets } from "@mysten/wallet-standard";
+import { getWallets, Wallet, signTransaction } from "@mysten/wallet-standard";
+import { getFullnodeUrl, SuiClient } from "@mysten/sui/client";
+import { Transaction } from "@mysten/sui/transactions";
+
+// Create and append login button to the DOM
+const loginButton = document.createElement("button");
+loginButton.innerHTML = "Login with Google";
+loginButton.className = "login-button";
+document.body.appendChild(loginButton);
+
+// Create and append login button to the DOM
+const signButton = document.createElement("button");
+signButton.innerHTML = "Sign the Tx";
+signButton.className = "login-button";
+document.body.appendChild(signButton);
+
+const apiKey = import.meta.env.VITE_ENOKI_PRIVATE_KEY;
+const client = new SuiClient({ url: getFullnodeUrl("testnet") });
 
 const availableWallets = getWallets().get();
+const suiWallet = availableWallets.find(
+  (wallet) => wallet.name == "Sui Wallet",
+);
 
-console.log(availableWallets);
+// state
+let accounts = null;
+if (suiWallet) {
+  console.log(suiWallet.accounts);
+  loginButton.addEventListener("click", () => connectWallet(suiWallet));
+  signButton.addEventListener("click", () => signSuiTransaction(suiWallet));
+}
+
+async function connectWallet(wallet: Wallet) {
+  const connectResult = await wallet.features["standard:connect"].connect();
+
+  if (!!connectResult.accounts.length) accounts = connectResult.accounts;
+}
+
+async function signAndExecuteTransaction(wallet: Wallet) {
+  const tx = new Transaction();
+
+  tx.setSender(accounts[0].address);
+  // tx.setGasBudget(50000);
+
+  // const txJson = await tx.toJSON({ supportedIntents: [], client });
+  const { bytes, signature } = await signTransaction(wallet, {
+    transaction: {
+      async toJSON() {
+        return await tx.toJSON({
+          supportedIntents: [],
+          client,
+        });
+      },
+    },
+    account: accounts[0] as any,
+    chain: "sui:testnet",
+  });
+  const result = await client.executeTransactionBlock({
+    transactionBlock: bytes,
+    signature,
+  });
+
+  console.log({ result });
+}
